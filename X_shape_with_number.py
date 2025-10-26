645b3df97a28e16735ece8968bed86b041e18ec4
Harsh Raja <135194537+RajaJi007@users.noreply.github.com>
2025-10-26 08:46:39 +0530

    Create X_shape_with_number.py
    Add a script to print an X shape with numbers

---
+++ X_shape_with_number.py
@@ -0,0 +1,8 @@
+n = 7
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if j == i or j == n - i + 1:
+            print(i, end='')
+        else:
+            print(' ', end='')
+    print()
